7a85f2f3d1dacfc564c539d4dfe0127a21b71c09
Mario Enrique Martínez Luis <darkusminato@gmail.com>
2018-10-30 15:46:53 -0600

    algunos cambios

--- js/ventana.js
+++ js/ventana.js
@@ -7,13 +7,3 @@ function ocultar() {
     document.getElementById("sidebar").style.width = "0";
     document.getElementById("contenido").style.marginLeft = "0px";
 }
-
-function mostrarEmbebed() {
-    document.getElementById("mapsEmbebed").style.width = "500px";
-    document.getElementById("contenido").style.marginLeft = "500px";
-}
-
-function ocultarEmbebed() {
-    document.getElementById("mapsEmbebed").style.width = "0";
-    document.getElementById("contenido").style.marginLeft = "0px";
-}
